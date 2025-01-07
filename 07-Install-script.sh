d9eca1b43b030375567b084fcfdf551858a3c968
Vasimille <ranjit.vasimille@accenture.com>
2025-01-07 23:03:41 +0530

    message

--- 07-Install-script.sh
+++ 07-Install-script.sh
@@ -9,6 +9,7 @@ then
 fi
 
 dnf install mysql -y
+
 if [ $? -ne 0 ]
 then
     echo "sql install status...failure"
@@ -18,7 +19,8 @@ else
 fi
 
 dnf install git -y
-if[ $? -ne 0 ]
+
+if [ $? -ne 0 ]
 then
     echo "git install.....failur"
     exit 1
