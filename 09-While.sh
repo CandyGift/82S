756f15f118691610f0e494549b5e4311e46a842c
Vasimille <ranjit.vasimille@accenture.com>
2025-01-10 22:45:01 +0530

    message

--- 09-While.sh
+++ 09-While.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 path="/home/ec2-user/82S"
-log_file=$(echo $0 | cut -d "." -f1).log
-source=$(find $path -name "*.log" -mtime +14) &>>$log_file
+source=$(find $path -name "*.log") &>>$log_file
 while read -r file_delete
 do
  
- rm -rf $source
+ rm -rf $file_delete &>>$log_file
 done <<< $log_file
